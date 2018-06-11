6391b78ac95c87147053e71b3aca7a6df938faee
hule <hule@DESKTOP-T432E4D>
2018-06-11 23:54:23 +0800

    main net

--- up.sh
+++ up.sh
@@ -1,12 +1,8 @@
-#!/usr/bin/env bash
-set -o errexit
-set -o xtrace
-
-docker run -ti --detach --name eos-node \
+docker run -ti --detach --name full-node \
        -v `pwd`:/etc/nodeos -v /tmp/nodeos-data:/data \
        -p 8888:8888 -p 9876:9876 \
        johnnyzhao/eos:1.0.2.2 \
        /opt/eosio/bin/nodeos --data-dir=/data \
-                             --config-dir=`pwd` \
-                             --genesis-json=`pwd`/genesis.json \
+                             --config-dir=/etc/nodeos \
+                             --genesis-json=/etc/nodeos/genesis.json \
                              --p2p-listen-endpoint=127.0.0.1:65432
